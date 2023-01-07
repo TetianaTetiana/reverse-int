4ac563b2748c68dfd491016c44aff130faf28009
Tetiana Karpenko <kuzia2016@i.ua>
2023-01-07 18:09:03 +0200

    feat: implement 'reverse' function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let numberToStr = Math.abs(n).toString();
+  let result = '';
+  let i = numberToStr.length - 1;
+
+  for( i; i >= 0; i -= 1 ){
+    result += numberToStr[i];
+  }
+
+  return result;
 }
